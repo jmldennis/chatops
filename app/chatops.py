33e13e62d70437934d023785dc5aa5851d369c62
jmldennis <jmldennis@gmail.com>
2022-04-29 00:20:48 -0500

    working docker compose, had to force to ip address... probably going to cause issues

--- app/chatops.py
+++ app/chatops.py
@@ -2,6 +2,7 @@ import messenger
 import yaml
 import json
 import requests
+from flask import Flask, request, json
 
 #Get Webex API KEY from YAML File
 try:
@@ -12,7 +13,26 @@ except Exception:
     print("Yaml file incomplete or invalid format")
     raise
 
+app = Flask(__name__)
+port = 5005
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    """Receive a notification from Webex Teams and handle it"""
+    if request.method == 'GET':
+        return f'Request received on local port {port}'
 
 message = messenger.Messenger(config["webex_api_key"])
 
 message.post_message_email("jodennis@cisco.com","hi")
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=port)
